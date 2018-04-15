e2e40d164c305c51ee85cac4093a9d886642de62
GreatSKL <49362109@qq.com>
2018-04-15 16:50:39 +0800

    update css code, remove some useless code and element

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -1,18 +1,18 @@
 import { MatCardModule, MatInputModule, MatButtonModule,
   MatToolbarModule, MatIconModule, MatMenuModule,
-  MatSidenavModule, MatListModule } from '@angular/material';
+  MatSidenavModule, MatListModule, MatTableModule } from '@angular/material';
 import { NgModule } from '@angular/core';
 
 @NgModule({
   imports: [
     MatCardModule, MatInputModule, MatButtonModule,
     MatToolbarModule, MatIconModule, MatMenuModule,
-    MatSidenavModule, MatListModule
+    MatSidenavModule, MatListModule, MatTableModule
   ],
   exports: [
     MatCardModule, MatInputModule, MatButtonModule,
     MatToolbarModule, MatIconModule, MatMenuModule,
-    MatSidenavModule, MatListModule
+    MatSidenavModule, MatListModule, MatTableModule
   ],
   declarations: [
   ]
